d5332f535e9ae61747e69c8513da091bcac5b2b0
Starford0 <akehford007@gmail.com>
2024-01-24 17:10:37 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import { persistStore } from 'redux-persist'
 
-let myPersistorVariable = persistStore(store)
+let myPersistorVariable = persistStore(Store)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
